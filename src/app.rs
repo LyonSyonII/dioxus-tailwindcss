07b33c4b9e84483c87e49675d2665dd0a9ebdf6f
LyonSyonII <slimystudios@protonmail.com>
2023-03-24 00:29:59 +0100

    Example now works perfectly

--- src/app.rs
+++ src/app.rs
@@ -2,25 +2,63 @@
 use dioxus::{prelude::*};
 
 pub fn App(cx: Scope) -> Element {
+    let mut count = use_state(cx, || 0);
+
     let rsx = rsx!(
         div {
-            class: "w-2/3 h-screen grid grid-cols-1 md:grid-cols-3 items-center justify-evenly mx-auto animate-wave",
-            
-            img { 
-                class: "w-1/2 justify-self-center animate-wave",
-                src: "dioxus.svg"
+            class: "flex flex-col h-screen items-center",
+            div {
+                class: "grid grid-cols-2 w-1/4 min-w-fit place-items-center mt-60",
+                a {
+                    class: "hover:animate-dioxus",
+                    href: "https://dioxuslabs.com",
+                    target: "_blank",
+                    img {
+                        class: "h-44 min-h-44 hover:drop-shadow-blue transition-shadow",
+                        src: "dioxus.svg",
+                    }
+                }
+                a {
+                    class: "hover:animate-tailwind",
+                    href: "https://tailwindcss.com",
+                    target: "_blank",
+                    img {
+                        class: "w-44 min-w-44 hover:drop-shadow-blue transition-shadow",
+                        src: "tailwind.svg"
+                    }
+                }
             }
-            
-            img {
-                class: "w-1/3 justify-self-center",
-                src: "heart.svg"
+            h1 {
+                class: "py-16 font-bold text-5xl",
+                "Dioxus + Tailwind"
             }
-            img {
-                class: "w-2/3 justify-self-center animate-wave",
-                src: "tailwind.svg"
+            button {
+                class: "bg-gray-200 px-4 py-2 rounded-lg border border-white hover:border-indigo-500 active:scale-95 transition-all",
+                onclick: move |_| count += 1,
+                "count is {count}"
             }
+            p {
+                class: "pt-4",
+                "Edit " Code {"src/app.rs"} " and run " Code {"dioxus serve"} " to test Hot Reload"
+            }
+            p {
+                class: "text-slate-500 pt-16",
+                "Click on the Dioxus and Tailwind logos to learn more"
+            }
+
         }
+
     );
     
     cx.render(rsx)
+}
+
+#[inline_props]
+pub fn Code<'a>(cx: Scope<'a>, children: Element<'a>) -> Element<'a> {
+    cx.render(rsx! {
+        code {
+            class: "bg-gray-100 text-gray-900 rounded px-1 py-0.5 font-mono text-sm",
+            children
+        }
+    })
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,25 @@ module.exports = {
     ],
     theme: {
       extend: {
-        animation: {
-          wave: 'wave 1s ease-in-out infinite',
+        dropShadow: {
+          blue: "0 0 2em #01A7D5"
         },
         keyframes: {
-          wave: {
-            '0%': {
-              transform: 'translateY(0)'
-            },
-            '50%': {
-              transform: 'translateY(-5px)'
-            },
-            '100%': {
-              transform: 'translateY(0)'
-            }
+          dioxus: {
+            "0%, 100%": { transform: "translateY(0px)"},
+            "33%": { transform: "translateY(-10px)"},
+            "66%": { transform: "translateY(10px)"},
+          },
+          tailwind: {
+            "0%, 100%": { transform: "translateY(0px)"},
+            "33%": { transform: "translateY(10px)"},
+            "66%": { transform: "translateY(-10px)"},
           },
         },
+        animation: {
+          dioxus: "dioxus 1s ease-in-out",
+          tailwind: "tailwind 1s ease-in-out",
+        }
       },
     },
     variants: {},
