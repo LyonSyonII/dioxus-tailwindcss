09487b89abaa456b6cca80c6a0057e8b3d44b4a6
LyonSyonII <slimystudios@protonmail.com>
2023-03-23 21:08:43 +0100

    Added animation

--- src/app.rs
+++ src/app.rs
@@ -4,18 +4,19 @@ use dioxus::{prelude::*};
 pub fn App(cx: Scope) -> Element {
     let rsx = rsx!(
         div {
-            class: "h-screen grid grid-cols-3 items-center justify-evenly gap-8",
+            class: "w-2/3 h-screen grid grid-cols-1 md:grid-cols-3 items-center justify-evenly mx-auto animate-wave",
             
-            img {
-                class: "w-1/2 text-center",
+            img { 
+                class: "w-1/2 justify-self-center animate-wave",
                 src: "dioxus.svg"
             }
+            
             img {
-                class: "w-1/3",
+                class: "w-1/3 justify-self-center",
                 src: "heart.svg"
             }
             img {
-                class: "w-2/3",
+                class: "w-2/3 justify-self-center animate-wave",
                 src: "tailwind.svg"
             }
         }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
       './src/**/*.html',
       './src/**/*.css'
     ],
-    theme: {},
+    theme: {
+      extend: {
+        animation: {
+          wave: 'wave 1s ease-in-out infinite',
+        },
+        keyframes: {
+          wave: {
+            '0%': {
+              transform: 'translateY(0)'
+            },
+            '50%': {
+              transform: 'translateY(-5px)'
+            },
+            '100%': {
+              transform: 'translateY(0)'
+            }
+          },
+        },
+      },
+    },
     variants: {},
     plugins: []
   }
